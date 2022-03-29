6ce6edd6be4068be43c5273d4fe988dab1cf6923
jiwon <alice010503@gmail.com>
2022-03-29 18:57:23 +0900

    problem 109 추가

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,6 +1,10 @@
 
 # -*- coding: euc-kr -*-
 # Problem 81
-string = "동해 물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세"
+'''string = "동해 물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세"
 print(len(string)-string.count(' '))
+'''
 
+# Problem 109
+tup = (1,2,3,4,5,6,7,8,9,10)
+print(sum(tup))
